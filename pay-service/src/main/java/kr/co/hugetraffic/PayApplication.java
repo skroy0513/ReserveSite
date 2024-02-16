9eb10e9fe2c183ccc09d81e1368bb0eff6bb6acd
skroy0513 <skroy0513@gmail.com>
2024-02-16 17:45:10 +0900

    pay-service 모듈 생성

---
+++ pay-service/src/main/java/kr/co/hugetraffic/PayApplication.java
@@ -0,0 +1,14 @@
+package kr.co.hugetraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
