60b22618c79d507d26460ac2a561cc835ccbfd22
skroy0513 <skroy0513@gmail.com>
2024-02-17 14:09:48 +0900

    order entity 생성

---
+++ order-service/src/main/java/kr/co/hugetraffic/entity/Order.java
@@ -0,0 +1,34 @@
+package kr.co.hugetraffic.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Builder
+@Data
+@Table(name = "order")
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order extends BaseDateTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "order_status")
+    private String status;
+}
