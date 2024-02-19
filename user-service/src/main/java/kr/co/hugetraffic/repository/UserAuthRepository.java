5aa27b729c478c26441d3da0205b374bec9fa2ee
skroy0513 <skroy0513@gmail.com>
2024-02-19 13:41:43 +0900

    user-service모듈 삭제

--- user-service/src/main/java/kr/co/hugetraffic/repository/UserAuthRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.hugetraffic.repository;
-
-import kr.co.hugetraffic.entity.UserAuth;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
-}
